cdcb3d2051d99cf01b6295d9fca63b7009a9fdf0
marceadecker <hello@marcea.de>
2015-04-21 13:53:27 -0400

    Initial Commit

---
+++ inClass-TableViewBluetoothChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  inClass-TableViewBluetoothChat
+//
+//  Created by Marcea Decker on 4/21/15.
+//  Copyright (c) 2015 md. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
